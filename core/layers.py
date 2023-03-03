2d3073a672e4699bcc04bd8a27ec68b24abee3aa
GongLiTian <gongc9@qq.com>
2023-03-03 22:05:35 +0800

    Changes to be committed: 	modified:   core/layers.py Changes not staged for commit: 	modified:   examples/mnist/run.py 	modified:   utils/dataset.py

--- core/layers.py
+++ core/layers.py
@@ -15,14 +15,19 @@ class Layer(object):
         self.params, self.grads = {}, {}
         self.is_training = True
 
+    # note:override this in your layer
     def forward(self, inputs):
         raise NotImplementedError
 
     def set_phase(self, phase):
         self.is_training = True if phase == "TRAIN" else False
 
+    # todo pprint  parameters of layer
+    # note:you can override this
     def __repr__(self) -> str:
         return f""
+
+
 class Dense(Layer):
 
     def __init__(self,
@@ -31,7 +36,9 @@ class Dense(Layer):
                  w_init=XavierUniformInit(),
                  b_init=ZerosInit()):
         super().__init__("Linear")
-        self.initializers = {"w": w_init, "b": b_init} #为参数绑定初始化函数
+
+        # note:bind initialization function for this layer
+        self.initializers = {"w": w_init, "b": b_init}
         self.shapes = {"w": [num_in, num_out], "b": [1, num_out]}
         self.params = {"w": None, "b": None}
 
@@ -42,22 +49,37 @@ class Dense(Layer):
         self.inputs = None
 
     def forward(self, inputs):
-        # lazy initialize
-        #若在前向传播时参数未初始化，自动通过输入的shape来初始化w和b
+        # note:initialize parameters denpending on shape of inputs while this layer hasn't been initialized
         if not self.is_init:
             self._init_parameters(inputs.shape[1])
 
         self.inputs = inputs
+        # todo:change this
         return inputs @ self.params["w"] + self.params["b"]
 
+    # todo
     def _init_parameters(self, input_size):
         self.shapes["w"][0] = input_size
 
         self.params["w"] = self.initializers["w"](shape=self.shapes["w"])
         self.params["b"] = self.initializers["b"](shape=self.shapes["b"])
+
         self.params["w"].zero_grad()
         self.params["b"].zero_grad()
         self.is_init = True
+
+
+class Softmax(Layer):
+
+    def __init__(self,
+                 num_in=None,
+                 ):
+        super().__init__("Softmax")
+        self.params = {"w": None, "b": None}
+        self.inputs = None
+
+    def forward(self, inputs):
+        return ops.exp(inputs) / ops.sum(ops.exp(inputs), axis=0)
 
 
 class Activation(Layer):
@@ -80,7 +102,7 @@ class Sigmoid(Activation):
         super().__init__("Sigmoid")
 
     def func(self, x):
-        return 1.0 / (1.0 + np.exp(-x))
+        return 1.0 / (1.0 + ops.exp(-x))
 
 
 class Tanh(Activation):
